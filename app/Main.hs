21373f310e1e4f8d239216a44de2ce44529cb4dc
Jeremiah Peschka <jeremiah.peschka@gmail.com>
2018-05-21 10:06:14 -0700

    Cleans up strictness.
    - Reduces use of BangExpressions to force strictness.
    - Uses `nf` instead of `whnf` to force full normal form evaluation

--- app/Main.hs
+++ app/Main.hs
@@ -7,70 +7,25 @@ import Lib
 import System.Directory
 import Data.ByteString as B hiding (map, take)
 import Data.ByteString.Char8 as C8 (pack)
-import Data.ByteString.Search
 import Data.List
 import Criterion.Main
-
-books :: [String]
-books = [ "76-0.txt", "74-0.txt" ]
-
-{-
-singleWords :: [ByteString]
-singleWords = map C8.pack ["screw","successful","chin","adamant","tick","enchanted","wry","shaggy","panoramic","squash","soothe","time","stocking","melt","hushed","nod","needy","person","zealous","reaction","economic","macabre","public","cats","manage","tasteless","peaceful","unarmed","aberrant","leg","curvy","gamy","talented","abortive","value","whole","word","colour","hope","amusing","vest","discussion","birthday","sick","surprise","superficial","language","feeble","general","test","wail","gigantic","teeny","unbecoming","jittery","craven","quickest","carpenter","pleasant","dazzling","tart","nebulous","destruction","cooing","muddle","hard-to-find","silly","blot","nose","son","regret","quince","orange","whip","marble","sneeze","excite","cap","eggnog","pizzas","conscious","water","vengeful","fuel","things","previous","teaching","unusual","concerned","numerous","fill","rod","nail","payment","addicted","bone","bite-sized","brash","noisy","force"]
-
-search1 :: ByteString -> [Int]
-search1 = boyerSearchOne $! Data.List.head singleWords
-
-search2 :: ByteString -> [Int]
-search2 = boyerSearchMany $! take 2 singleWords
-
-search3 :: ByteString -> [Int]
-search3 = boyerSearchMany $! take 3 singleWords
-
-search4 :: ByteString -> [Int]
-search4 = boyerSearchMany $! take 4 singleWords
-
-search5 :: ByteString -> [Int]
-search5 = boyerSearchMany $! take 5 singleWords
-
-searchMany :: ByteString -> [Int]
-searchMany = boyerSearchMany singleWords
-
-krSO :: ByteString -> [(Int, [Int])]
-krSO = krSearchOne $! Data.List.head singleWords
-
-krSM2 :: ByteString -> [(Int, [Int])]
-krSM2 = krSearchMany $! take 2 singleWords
-
-krSM3 :: ByteString -> [(Int, [Int])]
-krSM3 = krSearchMany $! take 3 singleWords
-
-krSM4 :: ByteString -> [(Int, [Int])]
-krSM4 = krSearchMany $! take 4 singleWords
-
-krSM5 :: ByteString -> [(Int, [Int])]
-krSM5 = krSearchMany $! take 5 singleWords
-
-krSM :: ByteString -> [(Int, [Int])]
-krSM = krSearchMany singleWords
--}
 
 main :: IO ()
 main = do
   let !singleWords = map C8.pack ["screw","successful","chin","adamant","tick","enchanted","wry","shaggy","panoramic","squash","soothe","time","stocking","melt","hushed","nod","needy","person","zealous","reaction","economic","macabre","public","cats","manage","tasteless","peaceful","unarmed","aberrant","leg","curvy","gamy","talented","abortive","value","whole","word","colour","hope","amusing","vest","discussion","birthday","sick","surprise","superficial","language","feeble","general","test","wail","gigantic","teeny","unbecoming","jittery","craven","quickest","carpenter","pleasant","dazzling","tart","nebulous","destruction","cooing","muddle","hard-to-find","silly","blot","nose","son","regret","quince","orange","whip","marble","sneeze","excite","cap","eggnog","pizzas","conscious","water","vengeful","fuel","things","previous","teaching","unusual","concerned","numerous","fill","rod","nail","payment","addicted","bone","bite-sized","brash","noisy","force"]
 
-  let !search1 = boyerSearchOne $! Data.List.head singleWords
-  let !search2 = boyerSearchMany $! take 2 singleWords
-  let !search3 = boyerSearchMany $! take 3 singleWords
-  let !search4 = boyerSearchMany $! take 4 singleWords
-  let !search5 = boyerSearchMany $! take 5 singleWords
-  let !searchMany = boyerSearchMany singleWords
-  let !krSO = krSearchOne $! Data.List.head singleWords
-  let !krSM2 = krSearchMany $! take 2 singleWords
-  let !krSM3 = krSearchMany $! take 3 singleWords
-  let !krSM4 = krSearchMany $! take 4 singleWords
-  let !krSM5 = krSearchMany $! take 5 singleWords
-  let !krSM = krSearchMany singleWords
+  let search1 = boyerSearchOne $ Data.List.head singleWords
+  let search2 = boyerSearchMany $ take 2 singleWords
+  let search3 = boyerSearchMany $ take 3 singleWords
+  let search4 = boyerSearchMany $ take 4 singleWords
+  let search5 = boyerSearchMany $ take 5 singleWords
+  let searchMany = boyerSearchMany $ take 10 singleWords
+  let krSO = krSearchOne $ Data.List.head singleWords
+  let krSM2 = krSearchMany $ take 2 singleWords
+  let krSM3 = krSearchMany $ take 3 singleWords
+  let krSM4 = krSearchMany $ take 4 singleWords
+  let krSM5 = krSearchMany $ take 5 singleWords
+  let krSM = krSearchMany $ take 10 singleWords
 
   getHomeDirectory >>= setCurrentDirectory
   makeAbsolute "./Downloads" >>= setCurrentDirectory
@@ -79,39 +34,39 @@ main = do
   defaultMain
     [ bgroup
         "boyer-moore/HuckFinn"
-        [ bench "search1" $ whnf search1 book1
-        , bench "search2" $ whnf search2 book1
-        , bench "search3" $ whnf search3 book1
-        , bench "search4" $ whnf search4 book1
-        , bench "search5" $ whnf search5 book1
-        , bench "searchMany" $ whnf searchMany book1
+        [ bench "search1" $ nf search1 book1
+        , bench "search2" $ nf search2 book1
+        , bench "search3" $ nf search3 book1
+        , bench "search4" $ nf search4 book1
+        , bench "search5" $ nf search5 book1
+        , bench "searchMany" $ nf searchMany book1
         ]
     , bgroup
         "karp-rabin/HuckFinn"
-        [ bench "krSearchOne" $ whnf krSO book1
-        , bench "krSM2" $ whnf krSM2 book1
-        , bench "krSM3" $ whnf krSM3 book1
-        , bench "krSM4" $ whnf krSM4 book1
-        , bench "krSM5" $ whnf krSM5 book1
-        , bench "krSearchMany" $ whnf krSM book1
+        [ bench "krSearchOne" $ nf krSO book1
+        , bench "krSM2" $ nf krSM2 book1
+        , bench "krSM3" $ nf krSM3 book1
+        , bench "krSM4" $ nf krSM4 book1
+        , bench "krSM5" $ nf krSM5 book1
+        , bench "krSearchMany" $ nf krSM book1
         ]
     , bgroup
         "boyer-moore/WarAndPeace"
-        [ bench "search1" $ whnf search1 book2
-        , bench "search2" $ whnf search2 book2
-        , bench "search3" $ whnf search3 book2
-        , bench "search4" $ whnf search4 book2
-        , bench "search5" $ whnf search5 book2
-        , bench "searchMany" $ whnf searchMany book2
+        [ bench "search1" $ nf search1 book2
+        , bench "search2" $ nf search2 book2
+        , bench "search3" $ nf search3 book2
+        , bench "search4" $ nf search4 book2
+        , bench "search5" $ nf search5 book2
+        , bench "searchMany" $ nf searchMany book2
         ]
     , bgroup
         "karp-rabin/WarAndPeace"
-        [ bench "krSearchOne" $ whnf krSO book2
-        , bench "krSM2" $ whnf krSM2 book2
-        , bench "krSM3" $ whnf krSM3 book2
-        , bench "krSM4" $ whnf krSM4 book2
-        , bench "krSM5" $ whnf krSM5 book2
-        , bench "krSearchMany" $ whnf krSM book2
+        [ bench "krSearchOne" $ nf krSO book2
+        , bench "krSM2" $ nf krSM2 book2
+        , bench "krSM3" $ nf krSM3 book2
+        , bench "krSM4" $ nf krSM4 book2
+        , bench "krSM5" $ nf krSM5 book2
+        , bench "krSearchMany" $ nf krSM book2
         ]
     ]
   {-
